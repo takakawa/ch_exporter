2b608aff2f123cfbcae0632de5f6aead8dcae82d
takakawa <takakawa@163.com>
2024-04-24 13:34:19 +0800

    first runnable demo

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,51 @@
-use std::net::SocketAddr;
-use std::str::FromStr;
-use std::{ thread};
-use std::time::Duration;
 
-use metrics::{counter, describe_counter, describe_histogram, gauge, histogram};
+use reqwest;
+use actix_web::{ web, App, get, HttpServer,HttpResponse};
+use config::{Config,FileFormat,File};
+mod qry;
 
-use metrics_exporter_prometheus::PrometheusBuilder;
-use metrics_util::MetricKindMask;
 
-use quanta::Clock;
-use rand::{thread_rng, Rng};
+#[get("/metrics")]
+async fn metrics(quries: web::Data<qry::Quries>) -> String {
+    let client = reqwest::Client::new();
+    let mut merge_resp = String::new();
 
-fn main() {
+    for     q in &quries.queries {
 
     
-    let builder = PrometheusBuilder::new();
-    builder
-        .with_http_listener(SocketAddr::from_str("127.0.0.1:8080").unwrap())
-        .idle_timeout(
-            MetricKindMask::COUNTER | MetricKindMask::HISTOGRAM,
-            Some(Duration::from_secs(10)),
-        )
-        .install()
-        .expect("failed to install Prometheus recorder");
+        let resp = client.get("http://127.0.0.1:8123")
+        .header("x-clickhouse-user", "ck")
+        .header("x-clickhouse-key", "M4XZ5biK")
+        .query(&[("query",&q.query)])
+        .send().await.unwrap().text().await.unwrap();
+        merge_resp.push_str(&resp);
 
-    // We register these metrics, which gives us a chance to specify a description for them.  The
-    // Prometheus exporter records this description and adds it as HELP text when the endpoint is
-    // scraped.
-    //
-    // Registering metrics ahead of using them is not required, but is the only way to specify the
-    // description of a metric.
-    describe_counter!("tcp_server_loops", "The iterations of the TCP server event loop so far.");
-    describe_histogram!(
-        "tcp_server_loop_delta_secs",
-        "The time taken for iterations of the TCP server event loop."
-    );
-
-    let clock = Clock::new();
-    let mut last = None;
-
-    counter!("idle_metric").increment(1);
-    gauge!("testing").set(42.0);
-
-    // Loop over and over, pretending to do some work.
-    loop {
-        counter!("tcp_server_loops", "system" => "foo").increment(1);
-
-        if let Some(t) = last {
-            let delta: Duration = clock.now() - t;
-            histogram!("tcp_server_loop_delta_secs", "system" => "foo").record(delta);
-        }
-
-        let increment_gauge = thread_rng().gen_bool(0.75);
-        let gauge = gauge!("lucky_iterations");
-        if increment_gauge {
-            gauge.increment(1.0);
-        } else {
-            gauge.decrement(1.0);
-        }
-
-        last = Some(clock.now());
-
-        thread::sleep(Duration::from_millis(750));
     }
+
+    merge_resp
+
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+
+    let mut builder = Config::builder()
+
+    .add_source(File::new("src/settings", FileFormat::Yaml)).build().unwrap();
+  
+
+    let config: qry::Quries = builder.try_deserialize().unwrap();
+    println!("config: {:?}",config);
+
+    let data = web::Data::new(config);
+    HttpServer::new(move || {
+        App::new()
+            .app_data(data.clone())
+            .service(metrics)
+            
+           
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
---
+++ src/qry.rs
@@ -0,0 +1,10 @@
+#[derive(Debug, Default, serde::Deserialize, Clone)]
+pub struct Query {
+    pub query: String,
+   
+}
+
+#[derive(Debug, Default, serde::Deserialize, Clone)]
+pub struct Quries {
+    pub queries : Vec<Query>
+}
